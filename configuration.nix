5bdf3cbe57d30463df5557e03fbcb1ef8ccc0c95
YourAlienFriend <119072116+YourAlienFriend@users.noreply.github.com>
2024-02-04 16:46:45 +0200

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -41,21 +41,10 @@
     networking.networkmanager.enable = true;
 
     # Set your time zone.
-    time.timeZone = "Europe/Athens";
+
 
     # Select internationalisation properties.
-    i18n.defaultLocale = "en_US.UTF-8";
 
-    i18n.extraLocaleSettings = {
-      LC_ADDRESS = "el_GR.UTF-8";
-      LC_IDENTIFICATION = "el_GR.UTF-8";
-      LC_MEASUREMENT = "el_GR.UTF-8";
-      LC_MONETARY = "el_GR.UTF-8";
-      LC_NAME = "el_GR.UTF-8";
-      LC_NUMERIC = "el_GR.UTF-8";
-      LC_PAPER = "el_GR.UTF-8";
-      LC_TELEPHONE = "el_GR.UTF-8";
-      LC_TIME = "el_GR.UTF-8";
     };
     
     # Enable the X11 windowing system.
